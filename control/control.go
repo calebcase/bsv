415f6f455569bd38a37413ea74a3e0b9368cb3a4
Caleb Case <calebcase@gmail.com>
2021-09-07 16:08:26 -0400

    control block

--- control/control.go
+++ control/control.go
@@ -1,24 +1,135 @@
 package control
 
+import (
+	"encoding/binary"
+	"io"
+)
+
 // Type is the control block type.
 type Type = byte
 
 // Control Block Types
 var (
-	Invalid Type = 0b0000_0000
-	Data    Type = 0b0000_0001
-	Size    Type = 0b0000_0010
-	Size2   Type = 0b0000_0100
-	Skip    Type = 0b0000_1000
+	Invalid      Type = 0b0000_0000
+	Data         Type = 0b0000_0001
+	DataSize     Type = 0b0000_0010
+	Data1        Type = 0b0000_0100
+	Data2        Type = 0b0000_1000
+	Skip         Type = 0b0001_0000
+	DataSizeSize Type = 0b0010_0000
+	SkipSizeSize Type = 0b0100_0000
+	Null         Type = 0b1000_0000
 )
 
 // Control Block Masks
 var (
-	dataMask  byte = 0b0000_0001
-	sizeMask  byte = 0b0000_0011
-	size2Mask byte = 0b0000_0111
-	skipMask  byte = 0b0000_1111
+	dataMask         byte = 0b0000_0001
+	dataSizeMask     byte = 0b0000_0011
+	data1Mask        byte = 0b0000_0111
+	data2Mask        byte = 0b0000_1111
+	skipMask         byte = 0b0001_1111
+	dataSizeSizeMask byte = 0b0011_1111
+	skipSizeSizeMask byte = 0b0111_1111
 )
+
+// TypeString returns the string name for the type.
+func TypeString(t Type) string {
+	switch t {
+	case Invalid:
+		return "Invalid"
+	case Data:
+		return "Data"
+	case DataSize:
+		return "Data Size"
+	case Data1:
+		return "Data + 1"
+	case Data2:
+		return "Data + 2"
+	case Skip:
+		return "Skip"
+	case DataSizeSize:
+		return "Data Size Size"
+	case SkipSizeSize:
+		return "Skip Size Size"
+	case Null:
+		return "Null"
+	}
+
+	return "Impossible"
+}
+
+// New returns a configured control byte.
+func New(t Type, value uint8) (b byte, err error) {
+	switch t {
+	case Invalid:
+		err = Error.New("refusing to create invalid block")
+
+		return
+	case Data:
+		if value > 127 {
+			err = Error.New("value too large: %d", value)
+
+			return
+		}
+
+		b = value<<1 | Data
+	case DataSize:
+		if value > 63 {
+			err = Error.New("value too large: %d", value)
+
+			return
+		}
+
+		b = value<<2 | DataSize
+	case Data1:
+		if value > 31 {
+			err = Error.New("value too large: %d", value)
+
+			return
+		}
+
+		b = value<<3 | Data1
+	case Data2:
+		if value > 15 {
+			err = Error.New("value too large: %d", value)
+
+			return
+		}
+
+		b = value<<4 | Data2
+	case Skip:
+		if value > 8 {
+			err = Error.New("value too large: %d", value)
+
+			return
+		}
+
+		value = value - 1
+		b = value<<5 | Skip
+	case DataSizeSize:
+		if value > 4 {
+			err = Error.New("value too large: %d", value)
+
+			return
+		}
+
+		value = value - 1
+		b = value<<6 | DataSizeSize
+	case SkipSizeSize:
+		if value > 2 {
+			err = Error.New("value too large: %d", value)
+
+			return
+		}
+
+		value = value - 1
+		b = value<<7 | SkipSizeSize
+	case Null:
+		b = Null
+	}
+
+	return
+}
 
 // Parse returns the control block type and value.
 func Parse(b byte) (t Type, value uint8, err error) {
@@ -26,19 +137,295 @@ func Parse(b byte) (t Type, value uint8, err error) {
 		v := b & ^dataMask >> 1
 
 		return Data, v, nil
-	} else if b&sizeMask == Size {
-		v := b & ^sizeMask >> 2
+	} else if b&dataSizeMask == DataSize {
+		v := b & ^dataSizeMask >> 2
 
-		return Size, v, nil
-	} else if b&size2Mask == Size2 {
-		v := b & ^size2Mask >> 3
+		return DataSize, v + 1, nil
+	} else if b&data1Mask == Data1 {
+		v := b & ^data1Mask >> 3
 
-		return Size2, v, nil
+		return Data1, v, nil
+	} else if b&data2Mask == Data2 {
+		v := b & ^data2Mask >> 4
+
+		return Data2, v, nil
 	} else if b&skipMask == Skip {
-		v := b & ^skipMask >> 4
+		v := b & ^skipMask >> 5
 
-		return Skip, v, nil
+		return Skip, v + 1, nil
+	} else if b&dataSizeSizeMask == DataSizeSize {
+		v := b & ^dataSizeSizeMask >> 6
+
+		return DataSizeSize, v + 1, nil
+	} else if b&skipSizeSizeMask == SkipSizeSize {
+		v := b & ^skipSizeSizeMask >> 7
+
+		return SkipSizeSize, v + 1, nil
+	} else if b == Null {
+		return Null, 0, nil
 	}
 
 	return Invalid, 0, Error.New("invalid control byte: %08b", b)
+}
+
+// Block is a block.
+type Block struct {
+	Type Type
+	Data []byte
+	Size uint64
+}
+
+func (b *Block) resize(size uint64) {
+	if uint64(cap(b.Data)) < size {
+		b.Data = make([]byte, size)
+	} else {
+		b.Data = b.Data[:size]
+	}
+}
+
+// Decoder is a decoder.
+type Decoder struct {
+	r io.Reader
+}
+
+// Decode parses a block from the reader.
+func (d *Decoder) Decode(b *Block) (err error) {
+	cb := make([]byte, 1)
+
+	_, err = io.ReadFull(d.r, cb)
+	if err != nil {
+		return
+	}
+
+	t, v, err := Parse(cb[0])
+	if err != nil {
+		return
+	}
+
+	b.Type = t
+	b.Data = b.Data[:0]
+	b.Size = 0
+
+	switch t {
+	case Invalid:
+		panic("invalid block detected when an error should have prevented it")
+	case Data:
+		b.Data = append(b.Data, byte(v))
+	case DataSize:
+		b.resize(uint64(v))
+
+		_, err = io.ReadFull(d.r, b.Data)
+		if err != nil {
+			return
+		}
+	case Data1:
+		b.resize(2)
+
+		b.Data[0] = v
+		_, err = io.ReadFull(d.r, b.Data[1:])
+		if err != nil {
+			return
+		}
+	case Data2:
+		b.resize(3)
+
+		b.Data[0] = v
+		_, err = io.ReadFull(d.r, b.Data[1:])
+		if err != nil {
+			return
+		}
+	case Skip:
+		b.Size = uint64(v)
+	case DataSizeSize:
+		b.resize(uint64(v))
+
+		_, err = io.ReadFull(d.r, b.Data)
+		if err != nil {
+			return
+		}
+
+		b.Size = binary.LittleEndian.Uint64(b.Data)
+
+		b.resize(b.Size)
+
+		_, err = io.ReadFull(d.r, b.Data)
+		if err != nil {
+			return
+		}
+	case SkipSizeSize:
+		b.resize(uint64(v))
+
+		_, err = io.ReadFull(d.r, b.Data)
+		if err != nil {
+			return
+		}
+
+		b.Size = binary.LittleEndian.Uint64(b.Data)
+	case Null:
+		// Nothing to do in this case. The block is already in the
+		// right condition.
+	}
+
+	return
+}
+
+// Encoder is an encoder.
+type Encoder struct {
+	w io.Writer
+}
+
+// NewEncoder returns a new encoder.
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{
+		w: w,
+	}
+}
+
+// Encode write a block to the writer.
+func (e *Encoder) Encode(b *Block) (err error) {
+	switch b.Type {
+	case Invalid:
+		e.w.Write([]byte{Invalid})
+	case Data:
+		cb, err := New(b.Type, b.Data[0])
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write([]byte{cb})
+		if err != nil {
+			return err
+		}
+	case DataSize:
+		cb, err := New(b.Type, uint8(len(b.Data)))
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write([]byte{cb})
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write(b.Data)
+		if err != nil {
+			return err
+		}
+	case Data1:
+		cb, err := New(b.Type, b.Data[0])
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write([]byte{cb})
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write(b.Data[1:])
+		if err != nil {
+			return err
+		}
+	case Data2:
+		cb, err := New(b.Type, b.Data[0])
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write([]byte{cb})
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write(b.Data[1:])
+		if err != nil {
+			return err
+		}
+	case Skip:
+		cb, err := New(b.Type, uint8(b.Size))
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write([]byte{cb})
+		if err != nil {
+			return err
+		}
+	case DataSizeSize:
+		var bytes uint8
+		buf := make([]byte, 4)
+		size := len(b.Data)
+
+		if size < 1<<8 {
+			bytes = 1
+			buf[0] = uint8(size)
+			buf = buf[:1]
+		} else if size < 1<<16 {
+			bytes = 2
+			binary.LittleEndian.PutUint16(buf[:2], uint16(size))
+			buf = buf[:2]
+		} else if size < 1<<24 {
+			bytes = 3
+			binary.LittleEndian.PutUint32(buf[:4], uint32(size))
+			buf = buf[:3]
+		} else {
+			bytes = 4
+			binary.LittleEndian.PutUint32(buf[:4], uint32(size))
+		}
+
+		cb, err := New(b.Type, bytes)
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write([]byte{cb})
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write(buf)
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write(b.Data)
+		if err != nil {
+			return err
+		}
+	case SkipSizeSize:
+		var bytes uint8
+		buf := make([]byte, 2)
+
+		if b.Size <= 1<<8 {
+			bytes = 1
+			buf[0] = uint8(b.Size - 1)
+			buf = buf[:1]
+		} else {
+			bytes = 2
+			binary.LittleEndian.PutUint16(buf[:2], uint16(b.Size-1))
+			buf = buf[:2]
+		}
+
+		cb, err := New(b.Type, bytes)
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write([]byte{cb})
+		if err != nil {
+			return err
+		}
+
+		_, err = e.w.Write(buf)
+		if err != nil {
+			return err
+		}
+	case Null:
+		_, err = e.w.Write([]byte{Null})
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- control/control_test.go
+++ control/control_test.go
@@ -1,13 +1,14 @@
 package control
 
 import (
+	"bytes"
 	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestParse(t *testing.T) {
+func TestControl(t *testing.T) {
 	type TC struct {
 		b   byte
 		t   Type
@@ -16,6 +17,7 @@ func TestParse(t *testing.T) {
 	}
 
 	tcs := []TC{
+		// Zero values:
 		{
 			b:   0b0000_0001,
 			t:   Data,
@@ -24,22 +26,53 @@ func TestParse(t *testing.T) {
 		},
 		{
 			b:   0b0000_0010,
-			t:   Size,
-			v:   0,
+			t:   DataSize,
+			v:   1,
 			err: false,
 		},
 		{
 			b:   0b0000_0100,
-			t:   Size2,
+			t:   Data1,
 			v:   0,
 			err: false,
 		},
 		{
 			b:   0b0000_1000,
-			t:   Skip,
+			t:   Data2,
 			v:   0,
 			err: false,
 		},
+		{
+			b:   0b0001_0000,
+			t:   Skip,
+			v:   1,
+			err: false,
+		},
+		{
+			b:   0b0010_0000,
+			t:   DataSizeSize,
+			v:   1,
+			err: false,
+		},
+		{
+			b:   0b0100_0000,
+			t:   SkipSizeSize,
+			v:   1,
+			err: false,
+		},
+		{
+			b:   0b1000_0000,
+			t:   Null,
+			v:   0,
+			err: false,
+		},
+		{
+			b:   0b0000_0000,
+			t:   Invalid,
+			v:   0,
+			err: true,
+		},
+		// Non-zero Values:
 		{
 			b:   0b1010_1011,
 			t:   Data,
@@ -48,41 +81,155 @@ func TestParse(t *testing.T) {
 		},
 		{
 			b:   0b1010_1010,
-			t:   Size,
-			v:   42,
+			t:   DataSize,
+			v:   43,
 			err: false,
 		},
 		{
 			b:   0b1010_1100,
-			t:   Size2,
+			t:   Data1,
 			v:   21,
 			err: false,
 		},
 		{
 			b:   0b1010_1000,
-			t:   Skip,
+			t:   Data2,
 			v:   10,
 			err: false,
 		},
 		{
+			b:   0b1011_0000,
+			t:   Skip,
+			v:   6,
+			err: false,
+		},
+		{
 			b:   0b1010_0000,
-			t:   Invalid,
-			v:   0,
-			err: true,
+			t:   DataSizeSize,
+			v:   3,
+			err: false,
+		},
+		{
+			b:   0b1100_0000,
+			t:   SkipSizeSize,
+			v:   2,
+			err: false,
 		},
 	}
 
 	for _, tc := range tcs {
 		t.Run(fmt.Sprintf("%08b", tc.b), func(t *testing.T) {
-			typ, v, err := Parse(tc.b)
-			require.Equal(t, tc.t, typ)
-			require.Equal(t, tc.v, v)
+			t.Run("parse", func(t *testing.T) {
+				typ, v, err := Parse(tc.b)
+				require.Equal(t, tc.t, typ)
+				require.Equal(t, tc.v, v)
 
-			if tc.err {
-				require.Error(t, err)
-			} else {
-				require.NoError(t, err)
-			}
+				if tc.err {
+					require.Error(t, err)
+				} else {
+					require.NoError(t, err)
+				}
+			})
+			t.Run("new", func(t *testing.T) {
+				b, err := New(tc.t, tc.v)
+				require.Equal(t, tc.b, b)
+
+				if tc.err {
+					require.Error(t, err)
+				} else {
+					require.NoError(t, err)
+				}
+			})
+		})
+	}
+}
+
+func TestEncode(t *testing.T) {
+	type TC struct {
+		blk  *Block
+		data []byte
+	}
+
+	tcs := []TC{
+		{
+			blk: &Block{
+				Type: Data,
+				Data: []byte{0b0000_0000}, // 0
+			},
+			data: []byte{0b0000_0001},
+		},
+		{
+			blk: &Block{
+				Type: Data,
+				Data: []byte{0b0101_0101}, // 85
+			},
+			data: []byte{0b1010_1011},
+		},
+		{
+			blk: &Block{
+				Type: Data,
+				Data: []byte{0b0111_1111}, // 127
+			},
+			data: []byte{0b1111_1111},
+		},
+		{
+			blk: &Block{
+				Type: DataSize,
+				Data: []byte{}, // E.g. zero length string
+			},
+			data: []byte{0b0000_0010},
+		},
+		{
+			blk: &Block{
+				Type: DataSize,
+				Data: []byte{'a', 'b', 'c', 'd'},
+			},
+			data: []byte{0b0001_0010, 'a', 'b', 'c', 'd'},
+		},
+		{
+			blk: &Block{
+				Type: Data1,
+				Data: []byte{0x1f, 0xff}, // 8_191
+			},
+			data: []byte{0b1111_1100, 0xff},
+		},
+		{
+			blk: &Block{
+				Type: Data2,
+				Data: []byte{0x0f, 0xff, 0xff}, // 1_048_575
+			},
+			data: []byte{0b1111_1000, 0xff, 0xff},
+		},
+		{
+			blk: &Block{
+				Type: Skip,
+				Size: 8,
+			},
+			data: []byte{0b1111_0000},
+		},
+		{
+			blk: &Block{
+				Type: DataSizeSize,
+				Data: []byte{'a', 'b', 'c', 'd'},
+			},
+			data: []byte{0b0010_0000, 0b0000_0100, 'a', 'b', 'c', 'd'},
+		},
+		{
+			blk: &Block{
+				Type: SkipSizeSize,
+				Size: 1 << 16, // 65536
+			},
+			data: []byte{0b1100_0000, 0xff, 0xff},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(fmt.Sprintf("%s %08b", TypeString(tc.blk.Type), tc.data), func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			enc := NewEncoder(buf)
+			err := enc.Encode(tc.blk)
+			require.NoError(t, err)
+			require.Equal(t, tc.data, buf.Bytes())
 		})
 	}
 }
